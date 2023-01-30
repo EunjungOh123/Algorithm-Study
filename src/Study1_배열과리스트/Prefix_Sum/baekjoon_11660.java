08d62f1e0b7e31a3e220d8976e331d908aee7af0
EunjunOh123 <grace99985@gmail.com>
2023-01-31 00:51:55 +0900

    commit: 구간 합 구하기 2 다른 풀이

--- src/Study1_배열과리스트/Prefix_Sum/baekjoon_11660.java
+++ src/Study1_배열과리스트/Prefix_Sum/baekjoon_11660.java
@@ -7,7 +7,8 @@ import java.util.StringTokenizer;
 
 public class baekjoon_11660 {
 
-    public static void main(String[] args) throws IOException {
+    /**
+     * public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
@@ -34,6 +35,44 @@ public class baekjoon_11660 {
             for(int j = x1; j <= x2; j++) {
                 ans += sumArr[j][y2] - sumArr[j][y1-1];
             }
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+    }
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n + 1][n + 1];
+        int[][] sumArr = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sumArr[i][j] = arr[i][j] + sumArr[i][j - 1] + sumArr[i - 1][j] - sumArr[i - 1][j - 1];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            long ans = 0;
+            st = new StringTokenizer(br.readLine(), " ");
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            ans += sumArr[x2][y2] - sumArr[x2][y1 - 1] - sumArr[x1 - 1][y2] + sumArr[x1 - 1][y1 - 1];
             sb.append(ans).append("\n");
         }
         System.out.println(sb);
