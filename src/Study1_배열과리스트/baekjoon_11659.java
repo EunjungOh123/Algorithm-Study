dbf0314d025a9e68499adea58b96e8c7f9032ab5
EunjunOh123 <grace99985@gmail.com>
2023-01-30 17:59:04 +0900

    commit: 구간 합 구하기 4

---
+++ src/Study1_배열과리스트/baekjoon_11659.java
@@ -0,0 +1,40 @@
+package Study1_배열과리스트;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 합 배열 생성 > 구간 합 (a번째 ~ b번째) : sumArr[b] - sumArr[a-1]
+ */
+
+public class baekjoon_11659 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n + 1];
+        long[] sumArr = new long[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sumArr[i] = sumArr[i - 1] + arr[i];
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            long ans = sumArr[b] - sumArr[a - 1];
+
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
