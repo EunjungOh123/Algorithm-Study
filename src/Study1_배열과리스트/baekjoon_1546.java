bea4534a213a44afafa108089ee9bd4a05d1fe40
EunjunOh123 <grace99985@gmail.com>
2023-01-30 16:37:49 +0900

    commit: 평균 구하기

---
+++ src/Study1_배열과리스트/baekjoon_1546.java
@@ -0,0 +1,28 @@
+package Study1_배열과리스트;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon_1546 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] scores = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(scores);
+        int max = scores[n - 1];
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += scores[i]  * 100 / (double) max;
+        }
+        double ans = sum/n;
+        System.out.println(ans);
+    }
+}
