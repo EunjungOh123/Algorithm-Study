8f012ca0f480fe72c45c6b95d3eb170e27b4e758
EunjunOh123 <grace99985@gmail.com>
2023-01-30 15:47:23 +0900

    commit: 숫자의 합 구하기

---
+++ src/Study1_배열과리스트/baekjoon_11720.java
@@ -0,0 +1,23 @@
+package Study1_배열과리스트;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * String > char [] 변환하는 방법도 있음 (문자 -> 숫자 변환: 문자 -'0')
+ */
+
+public class baekjoon_11720 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        String number = br.readLine();
+        int ans = 0;
+        for(int i = 0; i<cnt; i++) {
+            ans += Integer.parseInt(number.substring(i, i+1));
+        }
+        System.out.println(ans);
+    }
+}
