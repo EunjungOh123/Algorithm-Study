f55af738e12f05df156600f61b35bd387fb51abc
EunjunOh123 <grace99985@gmail.com>
2023-02-02 23:58:18 +0900

    commit: 부분합

---
+++ src/Study1_배열과리스트/Two_Pointer/baekjoon_1806.java
@@ -0,0 +1,48 @@
+package Study1_배열과리스트.Two_Pointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_1806 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        while (end <= n) {
+            int length = 0;
+            if (sum < s) {
+                sum += arr[end];
+                end++;
+            } else {
+                sum -= arr[start];
+                start++;
+            }
+            if (sum >= s) {
+                length = end - start;
+                if (length < min) {
+                    min = length;
+                }
+            }
+        }
+        if (min == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(min);
+        }
+    }
+}
